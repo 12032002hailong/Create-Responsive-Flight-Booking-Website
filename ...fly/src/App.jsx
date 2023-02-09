e1e78cc2d299ee174aa3ec9fc0a670ae3ebc7a18
Hai Long <hailongkc1203@gmail.com>
2023-02-09 18:04:22 +0700

    first commit

---
+++ ...fly/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Search from './Components/Search/Search';
+import Support from './Components/Support/Support';
+import Info from './Components/Info/info';
+import Travelers from './Components/Travelers/Travelers';
+import Lounge from './Components/Lounge/Lounge';
+import Subscriber from './Components/Subscribers/Subscriber';
+import Footer from './Components/Footer/Footer';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+      <Subscriber />
+      <Footer /> */}
+    </div>
+  )
+}
+
+export default App
